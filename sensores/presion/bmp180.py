1834a042172ff48702b4f92a08c040f3fcce2dba
Alvrimc1091 <89285922+Alvrimc1091@users.noreply.github.com>
2023-07-05 11:55:38 -0400

    Update bmp180.py

--- sensores/presion/bmp180.py
+++ sensores/presion/bmp180.py
@@ -7,9 +7,9 @@
 import bmpsensor
 import time
 while True:
-  temp, pressure, altitude = bmpsensor.readBmp180()
-  print("Temperature is ", temp) # degC
-  print("Pressure is ", pressure) # Pressure in Pa
-  print("Altitude is ", altitude) # Altitude in meters
-  print("\n")
-  time.sleep(2)
+    temp, pressure, altitude = bmpsensor.readBmp180()
+    print("Temperature is ",temp)  # degC
+    print("Pressure is ",pressure) # Pressure in Pa 
+    print("Altitude is ",altitude) # Altitude in meters
+    print("\n")
+    time.sleep(2)
