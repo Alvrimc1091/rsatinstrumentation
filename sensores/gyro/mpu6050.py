a25cf31d806d426715aff91abf759ec8472399d9
panchito <135763561+Tadeo2000@users.noreply.github.com>
2023-07-11 12:40:49 -0400

    Update mpu6050.py
    identation mistake

--- sensores/gyro/mpu6050.py
+++ sensores/gyro/mpu6050.py
@@ -84,26 +84,26 @@ with open(nombre_archivo, 'w', newline='') as archivo_csv:
     escritor_csv.writerow(['Gx[°/s]', 'Gy[°/s]', 'Gz[°/s]','Ax[g]', 'Ay[g]', 'Az[g]' ])  # Escribir encabezados de columna
 
     while True:
-        #Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
+	    #Read Accelerometer raw value
+	    acc_x = read_raw_data(ACCEL_XOUT_H)
+	    acc_y = read_raw_data(ACCEL_YOUT_H)
+	    acc_z = read_raw_data(ACCEL_ZOUT_H)
+		
+	    #Read Gyroscope raw value
+	    gyro_x = read_raw_data(GYRO_XOUT_H)
+	    gyro_y = read_raw_data(GYRO_YOUT_H)
+	    gyro_z = read_raw_data(GYRO_ZOUT_H)
+		
+	    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+	    Ax = acc_x/16384.0
+	    Ay = acc_y/16384.0
+	    Az = acc_z/16384.0
+	    
+	    Gx = gyro_x/131.0
+	    Gy = gyro_y/131.0
+	    Gz = gyro_z/131.0
+		
+	    escritor_csv.writerow([Gx, Gy, Gz,Ax, Ay, Az ])
+	    archivo_csv.flush()  # Vaciar el búfer y asegurarse de que se escriban los datos en el archivo
 	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
-	
-        escritor_csv.writerow([Gx, Gy, Gz,Ax, Ay, Az ])
-        archivo_csv.flush()  # Vaciar el búfer y asegurarse de que se escriban los datos en el archivo
-
-        time.sleep(1)  # Esperar 1 segundo antes de la siguiente captura
+	    time.sleep(1)  # Esperar 1 segundo antes de la siguiente captura
