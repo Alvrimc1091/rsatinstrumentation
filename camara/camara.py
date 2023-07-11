91b7f19614ffd917737e596574cbd65364816eb8
Alvrimc1091 <89285922+Alvrimc1091@users.noreply.github.com>
2023-07-11 13:46:02 -0400

    Update camara.py

--- camara/camara.py
+++ camara/camara.py
@@ -1,11 +1,14 @@
-
 import cv2
-
-cap = cv2.VideoCapture(0)
-
-# Capture frame
-ret, frame = cap.read()
-if ret:
-	cv2.imwrite('image.jpg', frame)
-
-cap.release()
+from picamera2 import Picamera2
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (1280,720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
+while True:
+    im= picam2.capture_array()
+    cv2.imshow("Camera", im)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cv2.destroyAllWindows()
