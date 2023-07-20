b915d7dce05e3c0fef92c2d16c0e776383d039d3
Alvrimc1091 <89285922+Alvrimc1091@users.noreply.github.com>
2023-07-20 17:14:36 -0400

    Update bmp180.py
    Hora agregada en primera columna en formato utc

--- sensores/presion/bmp180.py
+++ sensores/presion/bmp180.py
@@ -3,28 +3,15 @@
 # GND -> GND (PIN 6)
 # SCL -> SCL (PIN 5)
 # SDA -> SDA (PIN 3)
+
 import csv
 import bmpsensor
 import time
 import datetime
 import pytz
-# while True:
-#     temp, pressure, altitude = bmpsensor.readBmp180()
-#     print("Temperature is ",temp)  # degC
-#     print("Pressure is ",pressure) # Pressure in Pa 
-#     print("Altitude is ",altitude) # Altitude in meters
-#     print("\n")
-#     time.sleep(2)
-
-# Ejemplo de función para obtener los datos del sensor
-# def obtener_dato_sensor():
-#     # Aquí debes colocar el código para obtener los datos del sensor
-#     dato = 0.0  # Solo es un valor de ejemplo
-#     return dato
-
-# Nombre del archivo CSV
 nombre_archivo = 'data_bmp180.csv'
 zona_horaria_utc = pytz.utc
+
 # Ciclo de captura y escritura de datos
 with open(nombre_archivo, 'w', newline='') as archivo_csv:
     escritor_csv = csv.writer(archivo_csv)
@@ -32,10 +19,9 @@ with open(nombre_archivo, 'w', newline='') as archivo_csv:
 
     while True:
         temp, pressure, altitude = bmpsensor.readBmp180()
-        hora_actual = datetime.datetime.now(tz=zona_horaria_utc).strftime('%Y-%m-%d %H:%M:%S')
+        hora_actual = datetime.datetime.now(tz=zona_horaria_utc).strftime('%H%M%S')
 
         escritor_csv.writerow([hora_actual, temp, pressure, altitude])
         archivo_csv.flush()  # Vaciar el búfer y asegurarse de que se escriban los datos en el archivo
 
         time.sleep(1)  # Esperar 1 segundo antes de la siguiente captura
-
